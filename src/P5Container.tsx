028d223ed178cfb7aab77b28957b8a4bfcdb761d
David Young <tgsstdio@gmail.com>
2021-06-20 13:49:32 +1000

    p5 fill rect

--- src/P5Container.tsx
+++ src/P5Container.tsx
@@ -19,6 +19,36 @@ export default function P5Container() {
     }
   }
 
+  const renderRect = (p: any, info: any) => {
+    const {x, y, regionWidth, regionHeight, fillColor} = info
+
+    p.loadPixels();
+
+    const d = p.pixelDensity();
+    const screenWidth = p.width
+
+    // on 
+    const rowStride = 4 * (screenWidth * d) 
+    const xOffset = 4 * d * x;
+    const yOffset = d * y
+    const realWidth = 4 * d * regionWidth
+    const realHeight = d * regionHeight + yOffset
+
+    for(let i = yOffset; i < realHeight; i += 1) {
+      const alignment = i * rowStride + xOffset
+      for(let j = 0; j < realWidth; j += 4) {
+        const loc = alignment + j;
+
+        p.pixels[loc + 0] = p.red(fillColor);
+        p.pixels[loc + 1] = p.green(fillColor);
+        p.pixels[loc + 2] = p.blue(fillColor);
+        p.pixels[loc + 3] = p.alpha(fillColor);
+      }
+    }
+
+    p.updatePixels();
+  }
+
   const domRef = useCallback((node: HTMLElement|null) =>{
     if (node !== null) {
 
@@ -35,6 +65,57 @@ export default function P5Container() {
         g.draw = () => {
           g.background(220);
 
+          let pink = g.color(255, 102, 204);
+ 
+          let d = g.pixelDensity();
+
+          const screenWidth = g.width
+          const screenHeight = g.height
+        
+          const info = {
+            x: 50,
+            y: 25,
+            regionWidth: 100,
+            regionHeight: 100,
+            fillColor: pink
+          }
+
+          // fill path
+          renderRect(g, info)
+  
+          // g.loadPixels();
+
+          // // on 
+          // const rowStride = 4 * (screenWidth * d) 
+          // const xOffset = 4 * d * x;
+          // const yOffset = d * y
+          // const realWidth = 4 * d * regionWidth
+          // const realHeight = d * regionHeight + yOffset
+
+          // for(let i = yOffset; i < realHeight; i += 1) {
+          //   const alignment = i * rowStride + xOffset
+          //   for(let j = 0; j < realWidth; j += 4) {
+          //     const loc = alignment + j;
+
+          //     g.pixels[loc + 0] = g.red(pink);
+          //     g.pixels[loc + 1] = g.green(pink);
+          //     g.pixels[loc + 2] = g.blue(pink);
+          //     g.pixels[loc + 3] = g.alpha(pink);
+          //   }
+          // }
+
+          // g.updatePixels();
+
+
+
+          g.line(100, 25, 100, screenHeight/ 2);
+
+          g.line(0, screenHeight/ 2, screenWidth, screenHeight/ 2);
+
+          g.push();
+          // path match
+          g.translate(0, screenHeight/ 2)
+
           g.noFill();
 
           g.stroke(0, 0, 0);
@@ -47,22 +128,7 @@ export default function P5Container() {
           g.line(50, 25, 350, 25);
           g.line(50, 195, 350, 195);
 
-
-          let pink = g.color(255, 102, 204);
-          g.loadPixels();
-          let d = g.pixelDensity();
-          let regionWidth = g.width;
-          let regionHeight = g.height / 2;
-          let noOfPixels = 4 * (regionWidth * d) * (regionHeight * d);
-          let offset = noOfPixels;
-          let loopEnd = offset + noOfPixels
-          for (let i = offset; i < loopEnd; i += 4) {
-            g.pixels[i + 0] = g.red(pink);
-            g.pixels[i + 1] = g.green(pink);
-            g.pixels[i + 2] = g.blue(pink);
-            g.pixels[i + 3] = g.alpha(pink);
-          }
-          g.updatePixels();
+          g.pop();
         }
       }
 
